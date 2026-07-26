fdd0449937bf91e537e8f8bf303870b9c3b7cc62
Manases Lovera <manaseslovera07@gmail.com>
2026-07-25 21:31:05 -0400

    feat: exception excercise

--- 6.exceptions/Main.java
+++ 6.exceptions/Main.java
@@ -1,15 +1,34 @@
 public class Main {
     // Custom Exception Class exercise:
-    // Create a custom unchecked exception named 'InvalidAgeException' that extends RuntimeException.
+    // Create a custom unchecked exception named 'InvalidAgeException' that extends
+    // RuntimeException.
+    public static class InvalidAgeException extends RuntimeException {
+
+        public InvalidAgeException(String message) {
+            super(message);
+        }
+    }
+
+    public static boolean validateAge(int age) throws InvalidAgeException {
+
+        if (age >= 18) {
+            return true;
+        } else {
+            throw new InvalidAgeException("Invalid age");
+        }
+    }
 
     public static void main(String[] args) {
-        // EXERCISE 1: Custom Exception & Throwing
-        // 1. Create a method validateAge(int age) that throws InvalidAgeException if age < 18.
-        // 2. Call validateAge with invalid input inside a try-catch block and print the error message.
+        // EXERCISE: Custom Exception & Throwing
+        // 1. Create a method validateAge(int age) that throws InvalidAgeException if
+        // age < 18.
+        // 2. Call validateAge with invalid input inside a try-catch block and print the
+        // error message.
 
-        // EXERCISE 2: Try-With-Resources & AutoCloseable
-        // 1. Create a custom resource class 'DatabaseConnection' implementing AutoCloseable.
-        // 2. Implement the close() method to print "Connection closed automatically."
-        // 3. Use DatabaseConnection in a try-with-resources statement in main().
+        try {
+            validateAge(6);
+        } catch (InvalidAgeException e) {
+            System.out.println("There was an error: " + e.getMessage());
+        }
     }
 }
